4fb4c9c6346844483a3c16450542e63ad030723e
OkiruEll <elviseokiru@gmail.com>
2026-08-27 16:01:48 +0300

    Intro to Flask Routing

--- main.py
+++ main.py
@@ -3,3 +3,26 @@ from flask import Flask
 
 #A flask instance
 app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World!!"
+
+@app.route('/products')
+def products():
+    return "My Products"
+
+@app.route('/sales')
+def sales():
+    return "Sales"
+
+@app.route('/stock')
+def stock():
+    return "My Stock"
+
+@app.route('/dashboard')
+def dashboard():
+    return "Dashboard"
+
+
+app.run()
