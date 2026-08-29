c8b644d1d4b019967da53344c2f7ccd3c8647154
OkiruEll <elviseokiru@gmail.com>
2026-08-29 16:34:13 +0300

    NavBar Creation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , render_template
 
 
 #A flask instance
@@ -6,23 +6,31 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World!!"
+    return render_template('index.html')
 
 @app.route('/products')
 def products():
-    return "My Products"
+    return render_template('products.html')
 
 @app.route('/sales')
 def sales():
-    return "Sales"
+    return render_template('sales.html')
 
 @app.route('/stock')
 def stock():
-    return "My Stock"
+    return render_template('stock.html')
 
 @app.route('/dashboard')
 def dashboard():
-    return "Dashboard"
+    return render_template('dashboard.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 app.run()
